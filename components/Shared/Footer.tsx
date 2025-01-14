6e4d7c0dc5dfa837a6bce8c9e5d415d7feda449d
formixcode <formixcode@gmail.com>
2025-01-14 21:39:53 +0700

    make your project live and profitable

--- components/Shared/Footer.tsx
+++ components/Shared/Footer.tsx
@@ -7,21 +7,21 @@ export default function Footer() {
         <div className="row">
           <div className="col-lg-12">
             <div className="footer-top">
-              <p> Have a project in mind?</p>
+              <p>Make your project live and profitable</p>
               <h2>
-                <Link href="mailto:hello@websitename.com">Hello@noir.com</Link>
+                <Link href="mailto:contact@formixcode.com">contact@formixcode.com</Link>
               </h2>
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-6 col-sm-6">
-            <p className="copy-left-text">
+            {/* <p className="copy-left-text">
               Template by{" "}
               <Link href="https://themeforest.net/user/themeswolf/portfolio">
                 ThemesWolf
               </Link>
-            </p>
+            </p> */}
           </div>
           <div className="col-lg-6 col-sm-6">
             <p className="copy-right-text">&copy; Copyright 2025</p>
