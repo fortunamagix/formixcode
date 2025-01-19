052ee53e4b78d509433d14d5c17677985d74a655
formixcode <formixcode@gmail.com>
2025-01-20 01:51:25 +0700

    why us v1

--- components/Pages/OnePages/AboutMe.tsx
+++ components/Pages/OnePages/AboutMe.tsx
@@ -10,7 +10,7 @@ export default function AboutMe() {
         <div className="row">
           <div className="col-xl-12 col-lg-12">
             <div className="section-title section-white-title wow fadeInUp delay-0-2s">
-              <h2>About Me</h2>
+              <h2>Why Choose Us?</h2>
             </div>
           </div>
         </div>
@@ -22,22 +22,31 @@ export default function AboutMe() {
           <div className="col-lg-8 col-sm-12">
             <div className="about-content-part wow fadeInUp delay-0-2s">
               <p>
-                I’m Noir Walker, a seasoned UX designer with over 8 years of
-                experience in crafting intuitive and engaging digital
-                experiences. My journey began with a background in graphic
-                design, where I discovered my passion for understanding user
-                behavior and translating it into seamless interactions.
+              1. Expertise You Can Trust
+With years of experience in web development and design, we deliver cutting-edge, customized solutions tailored to your business needs.
+<br />
+<br />
+2. Affordable Yet Premium
+We offer high-quality services at half the price of traditional agencies, ensuring you get exceptional value for your investment.
+<br />
+<br />
+3. End-to-End Services
+From website design to hosting, maintenance, and integrations, we provide a seamless experience so you can focus on what matters—your business.
+<br />
+<br />
+4. Speed and Efficiency
+We prioritize fast-paced development and deliver results quickly without compromising quality.
               </p>
             </div>
             {/* <!-- START COUNTER DESIGN AREA --> */}
             <div className="hero-counter-area d-flex justify-content-between wow fadeInUp delay-0-4s">
               <div className="counter-item counter-text-wrap">
-                <CountUp className="count-text plus" start={0} end={8} enableScrollSpy />
+                <CountUp className="count-text plus" start={0} end={4} enableScrollSpy />
                 <span className="counter-title">Years Of Experience</span>
               </div>
               <div className="counter-item counter-text-wrap">
-              <CountUp className="count-text k-plus" start={0} end={1} enableScrollSpy />
-                <span className="counter-title">Complete Projects</span>
+              <CountUp className="count-text k-plus" start={0} end={200} enableScrollSpy />
+                <span className="counter-title">Value Delivery</span>
               </div>
               <div className="counter-item counter-text-wrap">
               <CountUp className="count-text percent" start={0} end={90} enableScrollSpy />
